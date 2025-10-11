1adc81614c0435001ac03ec2ed8061612f953fed
JSDeveloper17 <securemebro@gmail.com>
2025-10-12 02:12:55 +0530

    Update Backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api", // your backend URL
+  baseURL: "https://cms-backend-server-rd3e.onrender.com/api", 
 });
 
 // add token automatically if exists
